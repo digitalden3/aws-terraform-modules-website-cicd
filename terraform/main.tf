3363b50301c1d7ca72a18e95bec2beb72c1068d9
digitalden3 <denizyilmaz@hotmail.co.uk>
2023-11-27 07:13:11 +0000

    new

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,12 +23,3 @@ module "dns_acm" {
   root_domain                                         = var.root_domain
   dns_record_ttl                                      = var.dns_record_ttl
 }
-
-module "s3_website" {
-  source                                              = "./modules/s3_website"
-  website_bucket                                      = var.website_bucket
-  force_destroy                                       = var.force_destroy
-  versioning_enabled                                  = var.versioning_enabled
-  index_document                                      = var.index_document
-  region                                              = var.region
-}
